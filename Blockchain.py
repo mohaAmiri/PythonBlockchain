ca3a64d24af318197f22522132e07e3b79267534
unknown <amirimohammad117@gmail.com>
2023-10-15 15:40:18 +0330

    Calculate Balance

--- Blockchain.py
+++ Blockchain.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 from block import Block
 from transaction import Transaction
 from utility.hash_utils import hash_block
@@ -15,6 +17,7 @@ class Blockchain:
     def add_transaction(self, sender, recipient, amount):
         transaction = Transaction(sender, recipient, amount)
         self.open_transaction.append(transaction)
+        self.get_balance()
         return True
 
     def mine_block(self):
@@ -28,4 +31,25 @@ class Blockchain:
         new_block = Block(index, previous_hash, copied_transaction)
         self.chain.append(new_block)
         self.open_transaction = []
+        self.get_balance()
         return True
+
+    def get_balance(self):
+        participant = self.public_key
+        """ calculate total sent amount """
+        sent_chain_tx = [[tx.amount for tx in block.transactions if tx.sender == participant] for block in
+                         self.chain]
+        sent_op_tx = [tx.amount for tx in self.open_transaction if tx.sender == participant]
+        sent_chain_tx.append(sent_op_tx)
+        total_sent = reduce(lambda s, i: s + sum(i) if len(i) > 0 else s + 0,
+                            sent_chain_tx, 0)
+
+        """ calculate total received amount """
+        received_chain_tx = [[tx.amount for tx in block.transactions if tx.recipient == participant] for block in
+                             self.chain]
+        received_op_tx = [tx.amount for tx in self.open_transaction if tx.recipient == participant]
+        received_chain_tx.append(received_op_tx)
+        total_received = reduce(lambda s, i: s + sum(i) if len(i) > 0 else s + 0,
+                                received_chain_tx, 0)
+
+        return int(total_received - total_sent)
